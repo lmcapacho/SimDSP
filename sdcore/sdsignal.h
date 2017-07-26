e5f3f898532b2bc57235597e018e49360743506b
lmcapacho <lmcapacho@gmail.com>
2017-07-26 16:32:10 -0500

    Add copyright & date

--- sdcore/sdsignal.h
+++ sdcore/sdsignal.h
@@ -1,3 +1,26 @@
+/*
+ * SimDSP Signal.
+ *
+ * Copyright (c) 2017 Jorge I. Marin H.
+ *                    Alexander López Parrado
+ *                    Luis Miguel Capacho V.
+ *
+ * This file is part of SimDSP.
+ *
+ * SimDSP is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SimDSP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with SimDSP.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef SDSIGNAL_H
 #define SDSIGNAL_H
 
