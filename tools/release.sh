45396e6a748ec7ffa05c6cea636446a20d63e423
lmcapacho <lmcapacho@gmail.com>
2017-11-19 18:27:35 -0500

    Updated release script

--- tools/release.sh
+++ tools/release.sh
@@ -4,6 +4,36 @@
 
 arch_aux=`uname -m`
 
+echo ""
+echo "NOTE: Execute this script from outside the SimDSP folder"
+echo ""
+
+
+usage ()
+{
+  echo 'Usage : release.sh -version <version> -qt-path <qt_path>'
+  exit
+}
+
+if [ "$#" -ne 4 ]
+then
+  usage
+fi
+
+while [ "$1" != "" ]; do
+case "$1" in
+        "-version" )	shift
+						RELNAME=$1
+						;;
+        "-qt-path" )	shift
+                    	QT_HOME=$1
+                    	;;
+        * )         	echo "$1" " invalid option"
+        				exit
+    esac
+    shift
+done
+
 echo
 script_path="$(readlink -f "$0")"
 script_folder=$(dirname $script_path)
@@ -11,24 +41,12 @@ echo "script_folder $script_folder"
 
 current_dir=$(pwd)
 
-echo ""
-echo "NOTE: Don't forget to set this script's QT_HOME variable"
-echo "      Execute this script from outside the SimDSP folder"
-echo ""
-
 if [ "$arch_aux" == 'x86_64' ] ; then
   arch='AMD64'
-  QT_HOME="/home/lmcapacho/Qt/5.7/gcc_64"
+  QT_HOME=$QT_HOME"/gcc_64"
 else
   arch='i386'
-  QT_HOME="/home/lmcapacho/Qt/5.5/gcc"	
-fi
-
-if [ "$1" = "" ] ; then
-  echo "Usage: $0 <need a version string such as '1.0.0b' (without the quotes)>"
-  exit
-else
-  relname=$1
+  QT_HOME=$QT_HOME"/gcc"	
 fi
 
 app_folder=$(dirname $script_folder)
@@ -47,7 +65,7 @@ echo "Compiling SimDSP app..."
 $QT_HOME/bin/qmake -config release
 make
 
-release_name=simdsp-$relname.linux.$arch
+release_name=simdsp-$RELNAME.linux.$arch
 release_folder="$current_dir/$release_name"
 echo "making release folder: $release_folder"
 mkdir $release_folder
@@ -71,7 +89,7 @@ cp -rf $sdcore_folder/simdsp.h $sdcore_folder/sdfunctions.h include/
 echo "copying libraries"
 wget -c "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt*.AppImage
-./linuxdeployqt*.AppImage SimDSP -bundle-non-qt-libs
+./linuxdeployqt*.AppImage SimDSP -bundle-non-qt-libs -qmake=$QT_HOME/bin/qmake
 #linuxdeployqt SimDSP -bundle-non-qt-libs
 
 echo "copying sdcore libraries"
@@ -86,14 +104,14 @@ mv SimDSP lib/
 mv simdsp.sh simdsp
 chmod +x simdsp
 
-rm -f linuxdeployqt*.AppImage
+#rm -f linuxdeployqt*.AppImage
 
 cd $current_dir
 
-echo "compressing...."
-tar -cjf ./$release_name.tar.bz2 $release_name
+#echo "compressing...."
+#tar -cjf ./$release_name.tar.bz2 $release_name
 
-echo "cleaning up"
-rm -rf $release_folder
+#echo "cleaning up"
+#rm -rf $release_folder
 
 echo "done!"
