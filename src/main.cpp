e80690c9fcd0f4e5a8bdb31f82d7a726df970436
lmcapacho <lmcapacho@gmail.com>
2018-01-23 22:01:10 -0500

    Removed QInfoMsg in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -32,9 +32,6 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
         case QtDebugMsg:
             fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             break;
-        case QtInfoMsg:
-            fprintf(stderr, "Info: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
-            break;
         case QtWarningMsg:
             fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
             break;
